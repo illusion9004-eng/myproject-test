633a963360a45c8bdbaa15a64f5162b42b3db3e0
carami <carami@nate.com>
2026-04-16 15:36:31 +0900

    mysql 실습파일 추가

---
+++ mysql/0416.sql
@@ -0,0 +1,39 @@
+show tables;
+
+select * from jobs;
+
+select * from emp;
+
+desc emp;
+
+select empno, ename, job from emp;
+
+desc dept;
+
+select deptno, dname, loc from dept;
+select dname from dept;
+
+select * from emp;
+select * from dept;
+
+select version() ;
+select current_date;
+select 3+3; select version();
+select 
+deptno, dname
+from 
+dept;
+
+desc emp;
+
+select * from emp;
+select distinct job from emp;
+select ename, sal * 18 "연 봉" from emp;
+
+select * from emp 
+where sal > 2000;
+select * from emp where job='MANAGER';
+
+select * from emp where job='MANAGER' order by sal desc;
+show tables;
+
