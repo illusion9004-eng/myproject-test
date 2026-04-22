ca4cbab324bcee13ce16d8d2d08dc19a4bebaad1
carami <carami@nate.com>
2026-04-22 13:35:51 +0900

    update exam

--- happyjdbc/src/main/java/lion/jdbc/InsertExam.java
+++ happyjdbc/src/main/java/lion/jdbc/InsertExam.java
@@ -5,15 +5,9 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 
 public class InsertExam {
-    public boolean insertMember(String name, String email, String password){
-
-//        쓰기 불편해요.  성능도 떨어져요.
-        String sql = "insert into member(name,email, password) values('"+name+"','"+email+"','"+password+"')";
-
-
-        return false;
-    }
-    public static void main(String[] args) throws Exception{
+    //접근제한자 리턴타입 메소드명(매개변수들..) {}
+    public boolean insertMember(String name, String email, String pw) throws Exception{
+        boolean resultFlag = false;
         //1. 접속
         Connection conn = null;
         String url = "jdbc:mysql://localhost:3306/liondb";
@@ -28,14 +22,55 @@ public class InsertExam {
 //        String sql = "insert into member(name,email, password) values('kkk','kang@gmail.com','1111')";
         String sql = "insert into member(name,email, password) values(?,?,?)";
         PreparedStatement ps = conn.prepareStatement(sql);
-        ps.setString(1,"kkk");
-        ps.setString(2,"kkk@gmail.com");
-        ps.setString(3,"1234");
+        ps.setString(1,name);
+        ps.setString(2,email);
+        ps.setString(3,pw);
 
         //3. 쿼리 실행
         int resultCount = ps.executeUpdate();
+        if(resultCount==1)
+            return true;
+        return  false;
+    }
 
-//        conn.commit();
-        System.out.println(resultCount + "건 입력됨!!");
+
+
+    public static void main(String[] args) {
+        InsertExam exam = new InsertExam();
+        try {
+            boolean result = exam.insertMember("강경미", "ccc@gmail.com", "1234");
+
+            if (result)
+                System.out.println("입력성공@@");
+            else
+                System.out.println("입력실패");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+
+//        //1. 접속
+//        Connection conn = null;
+//        String url = "jdbc:mysql://localhost:3306/liondb";
+//        String user = "lion";
+//        String password = "lion1234";
+//        conn = DriverManager.getConnection(url,user,password);
+//
+////        conn.setAutoCommit(false);   -- 기본 설정은 true
+//
+//
+//        //2. 쿼리문 작성 = 쿼리문이 추상화된 객체 Statement
+////        String sql = "insert into member(name,email, password) values('kkk','kang@gmail.com','1111')";
+//        String sql = "insert into member(name,email, password) values(?,?,?)";
+//        PreparedStatement ps = conn.prepareStatement(sql);
+//        ps.setString(1,"kkk");
+//        ps.setString(2,"kkk@gmail.com");
+//        ps.setString(3,"1234");
+//
+//        //3. 쿼리 실행
+//        int resultCount = ps.executeUpdate();
+//
+////        conn.commit();
+//        System.out.println(resultCount + "건 입력됨!!");
     }
 }
--- mysql/0421.sql
+++ mysql/0421.sql
@@ -10,4 +10,15 @@ set autocommit = 0;
 commit;
 rollback;
 
-update member set name = 'kkm' where id = 8;
+update member set name = 'kkm222' where id = 8;
+
+desc member;
+
+select * from member;
+select id, email, name from member;
+
+select version();
+
+
+insert into member(name,email, password) values('','kang@gmail.com','1111');
+
